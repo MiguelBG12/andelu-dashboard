c9d07cb93f18019e1865d8deb9e8aa4ef63358cd
MiguelBG12 <miguel.bg12@gmail.com>
2024-10-24 14:47:01 -0500

    Se añade la visualización de 'total' y 'average' con iconos dinámicos según el valor del promedio

--- app/(routes)/components/CardSummary/CardSummary.tsx
+++ app/(routes)/components/CardSummary/CardSummary.tsx
@@ -1,6 +1,8 @@
 import { CustomIcon } from "@/components/CustomIcon";
 import { CardSummaryProps } from "./CardSummary.types";
 import { CustomTooltip } from "@/components/CustomTooltip";
+import { cn } from "@/lib/utils";
+import { MoveDownRight, MoveUpLeft, MoveUpRight, TrendingUp } from "lucide-react";
 
 export function CardSummary(props: CardSummaryProps) {
   const { icon: Icon, total, average, title, tooltipText } = props;
@@ -12,6 +14,23 @@ export function CardSummary(props: CardSummaryProps) {
           {title}
         </div>
         <CustomTooltip content={tooltipText} />
+      </div>
+      <div className="flex gap4 mt-2 md:mt-4">
+        <p className="text2xl">{total}</p>
+        <div className={cn(`flex items-center gap-1 px-2 text-xs text-white rounded-lg h-[20px] bg-black dark:bg-secondary`)}>
+          {average}%
+
+          {average < 20 && (
+            <MoveDownRight strokeWidth={2} className="h-4 w-4" />
+          )}
+          {average > 20 && average < 70 && (
+            <MoveUpRight strokeWidth={2} className="h-4 w-4" />
+          )}
+          {average > 70 && average < 100 && (
+            <TrendingUp strokeWidth={2} className="h-4 w-4" />
+          )}
+        </div>
+
       </div>
     </div>
   );
