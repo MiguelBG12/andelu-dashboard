2a2d1e73f16d7fa5d4b9dc9949d87828c19bb36c
MiguelBG12 <miguel.bg12@gmail.com>
2024-10-24 14:00:16 -0500

    Se integra TooltipProvider y TooltipContent para mostrar información detallada en CustomTooltip.

--- components/CustomTooltip/CustomTooltip.tsx
+++ components/CustomTooltip/CustomTooltip.tsx
@@ -1,9 +1,26 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
 import { CustomTooltipProps } from "./CustomTooltip.types";
+import { Info } from "lucide-react";
 
 export function CustomTooltip(props: CustomTooltipProps) {
-    const {content} = props
-    
+  const { content } = props;
+
   return (
-    <div>CustomTooltip</div>
-  )
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger>
+          <Info strokeWidth={1} className="h-5 w-5" />
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>{content}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
 }
