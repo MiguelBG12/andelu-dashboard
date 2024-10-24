dc557578e5f4030ffad5932789e727037850302c
MiguelBG12 <miguel.bg12@gmail.com>
2024-10-24 13:59:52 -0500

    Se añade CustomTooltip para mejorar la usabilidad con descripciones adicionales en CardSummary

--- app/(routes)/components/CardSummary/CardSummary.tsx
+++ app/(routes)/components/CardSummary/CardSummary.tsx
@@ -1,13 +1,17 @@
 import { CustomIcon } from "@/components/CustomIcon";
 import { CardSummaryProps } from "./CardSummary.types";
+import { CustomTooltip } from "@/components/CustomTooltip";
 
 export function CardSummary(props: CardSummaryProps) {
   const { icon: Icon, total, average, title, tooltipText } = props;
   return (
     <div className="shadow-sm bg-background rounded-lg p-5 py-3 hover:shadow-kg transition">
       <div className="flex justify-between">
-        <CustomIcon icon={Icon}/>
-        {title}
+        <div className="flex gap-2 items-center">
+          <CustomIcon icon={Icon} />
+          {title}
+        </div>
+        <CustomTooltip content={tooltipText} />
       </div>
     </div>
   );
