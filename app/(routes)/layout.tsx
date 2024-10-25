200523180d8f2f7d76b0e62e19b7c44b9207256b
MiguelBG12 <miguel.bg12@gmail.com>
2024-10-24 22:11:17 -0500

    Se reemplaza el texto del Navbar con un componente reutilizable para mejorar la modularidad del Layout.

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -1,3 +1,5 @@
+import { Navbar } from "@/components/Navbar";
+
 export default function LayoutDashboard({
   children,
 }: {
@@ -7,7 +9,7 @@ export default function LayoutDashboard({
     <div className="flex w-full h-full">
       <div className="hidden xl:block w-80 h-full xk:fixed">Sidebar</div>
       <div className="w-full xl:ml-80">
-        <p>Navbar</p>
+        <Navbar />
         <div className="p-6 bg-[#fafbfc] dark:bg-secundary">{children}</div>
       </div>
     </div>
