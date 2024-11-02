5b19bb603e10a56dbf0d18084f0e0f78d23582e4
MiguelBG12 <miguel.bg12@gmail.com>
2024-11-02 14:14:42 -0500

    Componente Sidebar con espacio reservado para el logo y las rutas, usando toda la altura de la pantalla."

---
+++ components/Sidebar/Sidebar.tsx
@@ -0,0 +1,13 @@
+import { SidebarRoutes } from "../SidebarRoutes";
+
+export function Sidebar() {
+  return (
+    <div className="h-screen">
+        <div className="h-full flex flex-col border-">
+            <p>Logo</p>
+            <SidebarRoutes />
+        </div>
+
+    </div>
+  )
+}
