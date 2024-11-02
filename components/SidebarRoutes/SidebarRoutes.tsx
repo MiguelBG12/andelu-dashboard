2c4b16c2161c8752ba0491bab91a42ae3bde8c76
MiguelBG12 <miguel.bg12@gmail.com>
2024-11-02 12:50:52 -0500

    Se organizan las secciones del sidebar en categorías cada una con su propio título y separador

--- components/SidebarRoutes/SidebarRoutes.tsx
+++ components/SidebarRoutes/SidebarRoutes.tsx
@@ -11,13 +11,29 @@ export function SidebarRoutes() {
     <div className="flex flex-col justify-between h-full">
       <div>
         <div className="p-2 md:p-6">
-            <p>GENERAL</p>
-            {dataGeneralSidebar.map((item) => (
-                <SidebarItem key={item.label} item={item}/>
-            ))}
+          <p className="text-slate-500 mb-2">GENERAL</p>
+          {dataGeneralSidebar.map((item) => (
+            <SidebarItem key={item.label} item={item} />
+          ))}
         </div>
 
         <Separator />
+
+        <div className="p-2 md:p-6">
+          <p className="text-slate-500 mb-2">TOOLS</p>
+          {dataToolsSidebar.map((item) => (
+            <SidebarItem key={item.label} item={item} />
+          ))}
+        </div>
+
+        <Separator />
+
+        <div className="p-2 md:p-6">
+          <p className="text-slate-500 mb-2">SUPPORT</p>
+          {dataSupportSidebar.map((item) => (
+            <SidebarItem key={item.label} item={item} />
+          ))}
+        </div>
       </div>
     </div>
   );
