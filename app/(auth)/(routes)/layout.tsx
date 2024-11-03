67850662dd0791fd7215c0c5e793c4a7fba4783c
MiguelBG12 <miguel.bg12@gmail.com>
2024-11-02 23:40:24 -0500

    Reemplazo del texto por el logo

--- app/(auth)/(routes)/layout.tsx
+++ app/(auth)/(routes)/layout.tsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import { Logo } from "@/components/Logo";
+import React from "react";
 
-export default function LayoutAuth({ children }: { children: React.ReactNode}) {
+export default function LayoutAuth({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <div className="flex flex-col justify-center h-full items-center">
-      <p>Andelu Dashboard</p>
-      <h1 className='text-3xl my-2'>Bienvenidos a mi Dashboard</h1>
-      <h2 className='text-2xl mb-3'>Dashboard</h2>
-      
+      <Logo />
+      <h1 className="text-3xl my-2">Bienvenidos a mi Dashboard</h1>
+      <h2 className="text-2xl mb-3">Dashboard</h2>
+
       {children}
     </div>
-    
-  )
+  );
 }
