246f128b8045d89008d015422d1de4a83c3d5b0f
MiguelBG12 <miguel.bg12@gmail.com>
2024-11-09 10:23:39 -0500

    Encabezado con botón para abrir un modal de creación de empresa

--- app/(routes)/companies/components/HeaderCompanies/HeaderCompanies.tsx
+++ app/(routes)/companies/components/HeaderCompanies/HeaderCompanies.tsx
@@ -1,7 +1,37 @@
-import React from 'react'
+"use client"
+
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { CirclePlus } from "lucide-react"
+import { useState } from "react"
 
 export function HeaderCompanies() {
+  const [openModalCreate, setOpenModalCreate] = useState(false)
+
   return (
-    <div>HeaderCompanies</div>
+    <div className="flex items-center justify-between">
+      <h2 className="text-2xl">List companies</h2>
+
+      <Dialog open={openModalCreate} onOpenChange={setOpenModalCreate}>
+        <DialogTrigger asChild>
+          <Button>Create Company</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[625px]">
+          <DialogHeader>
+            <DialogTitle>Create Customer</DialogTitle>
+            <DialogDescription>
+              Create and configure your customer
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </div>
   )
 }
