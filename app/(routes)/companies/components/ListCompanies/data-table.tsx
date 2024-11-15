b16e19311005f52a50d4d36efd894d9006b57af6
MiguelBG12 <miguel.bg12@gmail.com>
2024-11-14 22:37:21 -0500

    Se añaden headers a DataTable para soporte de columnas configurables y filtros dinámicos

--- app/(routes)/companies/components/ListCompanies/data-table.tsx
+++ app/(routes)/companies/components/ListCompanies/data-table.tsx
@@ -1,31 +1,98 @@
-"use client"
+"use client";
 
-import React from "react"
-
-import {
-    ColumnDef,
-    SortingState,
-    flexRender,
-    getCoreRowModel,
-    ColumnFiltersState,
-    getFilteredRowModel,
-    getSortedRowModel,
-    useReactTable,
-    getPaginationRowModel,
-} from "@tanstack/react-table"
+import React from "react";
 
 import {
-    Table,
-    TableBody,
-    TableCell,
-    TableHead,
-    TableRow,
-} from "@/components/ui/table"
+  ColumnDef,
+  SortingState,
+  flexRender,
+  getCoreRowModel,
+  ColumnFiltersState,
+  getFilteredRowModel,
+  getSortedRowModel,
+  useReactTable,
+  getPaginationRowModel,
+} from "@tanstack/react-table";
 
-import { Input } from "@/components/ui/input"
-import { ButtonProps } from "@/components/ui/button"
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+import { Input } from "@/components/ui/input";
+import { ButtonProps } from "@/components/ui/button";
 
 interface DataTableProps<TData, TValue> {
-    columns: ColumnDef<TData, TValue>[],
-    data: TData[]
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+}
+
+export function DataTable<TData, TValue>({
+  columns,
+  data,
+}: DataTableProps<TData, TValue>) {
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    []
+  );
+  const [isMounted, setIsMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
+    state: {
+      sorting,
+      columnFilters,
+    },
+  });
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <div className="p-4 bg-background shadow-md rounded-lg mt-4">
+      <div className="flex items-center mb-2">
+        <Input
+          placeholder="Filter company..."
+          value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
+          onChange={(event) =>
+            table.getColumn("name")?.setFilterValue(event.target.value)
+          }
+        />
+      </div>
+      <div className="rounded-md border">
+        <Table>
+            <TableHeader>
+                {table.getHeaderGroups().map((headerGroup) => (
+                    <TableRow key={headerGroup.id}>
+                        {headerGroup.headers.map((header) => {
+                            return (
+                                <TableHead key={headerGroup.id}>
+                                    {header.isPlaceholder ? null : flexRender(header.column.
+                                        columnDef.header, header.getContext())}
+                                </TableHead>
+                            )
+                        })}
+                    </TableRow>
+                ))}
+            </TableHeader>
+        </Table>
+      </div>
+    </div>
+  );
 }
