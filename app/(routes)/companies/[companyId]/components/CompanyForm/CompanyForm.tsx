7808f1a7c107069725c6b1dd6dcebf1f31aa44d7
MiguelBG12 <miguel.bg12@gmail.com>
2024-11-17 21:34:23 -0500

    Se añade la implementación completa del formulario CompanyForm

--- app/(routes)/companies/[companyId]/components/CompanyForm/CompanyForm.tsx
+++ app/(routes)/companies/[companyId]/components/CompanyForm/CompanyForm.tsx
@@ -7,23 +7,23 @@ import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
-import { Textarea } from "@/components/ui/textarea"
+import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-} from "@/components/ui/form"
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue
-} from "@/components/ui/select"
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { Input } from "@/components/ui/input";
 import { Toast } from "@/components/ui/toast";
 
@@ -31,31 +31,173 @@ import { UploadButton } from "@/utils/uploadthing";
 
 import { CompanyFormProps } from "./CompanyForm.types";
 import { formSchema } from "./CompanyForm.form";
+import { toast } from "@/hooks/use-toast";
 
 export function CompanyForm(props: CompanyFormProps) {
   const { company } = props;
-  const router = useRouter()
-  
-  const [photoUploaded, setPhotoUploaded] = useState(false)
+  const router = useRouter();
 
-  const form = useForm<z.infer<typeof formSchema>>( {
-    resolver:zodResolver(formSchema),
+  const [photoUploaded, setPhotoUploaded] = useState(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
     defaultValues: {
-        name: company.name,
-        description: company.description,
-        country: company.country,
-        website: company.website,
-        phone: company.phone,
-        RUC: company.RUC,
-        profileImage: company.profileImage,
-    }
-  })
+      name: company.name,
+      description: company.description,
+      country: company.country,
+      website: company.website,
+      phone: company.phone,
+      RUC: company.RUC,
+      profileImage: company.profileImage,
+    },
+  });
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log("on submit")
-  }
+    console.log("on submit");
+  };
 
   return (
-    <div>CompanyForm</div>
-  )
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <div className="grid grid-cols-2 gap-3">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Company name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Company name" type="text" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="country"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Country</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select country" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectItem value="argentina">Argentina</SelectItem>
+                    <SelectItem value="brasil">Brasil</SelectItem>
+                    <SelectItem value="usa">USA</SelectItem>
+                    <SelectItem value="chile">Chile</SelectItem>
+                    <SelectItem value="colombia">Colombia</SelectItem>
+                    <SelectItem value="peru">Peru</SelectItem>
+                    <SelectItem value="mexico">Mexico</SelectItem>
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="website"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Website</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="www.miguelbautista.com"
+                    type="text"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="phone"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Phone</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="+51 999 999 999"
+                    type="number"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="RUC"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>RUC</FormLabel>
+                <FormControl>
+                  <Input placeholder="20458575108" type="number" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="profileImage"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Profile Image</FormLabel>
+                <FormControl>
+                  <div>
+                    {photoUploaded ? (
+                      <p className="text-sm">Image uploaded</p>
+                    ) : (
+                      <UploadButton
+                        className="bg-slate-600/20 text-slate-800 rounded-lg
+                      outline-dotted outline-3"
+                        {...field}
+                        endpoint="profileImage"
+                        onClientUploadComplete={(res) => {
+                          form.setValue("profileImage", res?.[0].url);
+                          setPhotoUploaded(true);
+                        }}
+                        onUploadError={(error: Error) => {
+                          toast({ title: "Error uploading photo" });
+                        }}
+                      />
+                    )}
+                  </div>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Company description</FormLabel>
+                <FormControl>
+                  <Textarea
+                    placeholder="Description..."
+                    {...field}
+                    value={form.getValues().description ?? ""}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+        </div>
+        <Button type="submit">Edit company</Button>
+      </form>
+    </Form>
+  );
 }
