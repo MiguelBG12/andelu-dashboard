7bc57b0027e42fd4c4847d03dd2d9f774501b3f2
MiguelBG12 <miguel.bg12@gmail.com>
2024-11-18 17:16:41 -0500

    Se corrige el formulario inicial en FormContact para capturar datos del contacto

--- app/(routes)/companies/[companyId]/components/NewContact/FormContact/FormContact.tsx
+++ app/(routes)/companies/[companyId]/components/NewContact/FormContact/FormContact.tsx
@@ -26,50 +26,48 @@ import {
 import { Input } from "@/components/ui/input";
 import { Toast } from "@/components/ui/toast";
 
-
 import { FormContactProps } from "./FormContact.types";
 import { formSchema } from "./FormContact.form";
-
 
 export function FormContact(props: FormContactProps) {
   const { setOpen } = props;
 
-  const params = useParams<{companyId: string}>
-  const router = useRouter()
+  const params = useParams<{ companyId: string }>;
+  const router = useRouter();
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-        name: "",
-        role: "",
-        email: "",
-        phone: "",
-    }
-  })
+      name: "",
+      role: "",
+      email: "",
+      phone: "",
+    },
+  });
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log("On submit")
-  }
+    console.log("On submit");
+  };
 
   return (
     <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="grid gap-4 md:grid-cols">
-            <FormField
-            control={form.control}
-            name="name"
-            render={({field}) => (
-                <FormItem>
-                    <FormLabel>Name</FormLabel>
-                    <FormControl>
-                        <Input placeholder="Migue Bautista" {...field} />
-                    </FormControl>
-                </FormItem>
-            )}
-            >
-
-            </FormField>
-        </form>
-
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="grid gap-4 md:grid-cols"
+      >
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input placeholder="Migue Bautista" {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+      </form>
     </Form>
-  )
+  );
 }
