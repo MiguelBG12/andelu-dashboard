cb6b3edfb214ae2b54241c563f08f2a00360f32e
MiguelBG12 <miguel.bg12@gmail.com>
2024-11-19 14:46:08 -0500

    Listado de contactos con opciones de llamada y correo

--- app/(routes)/companies/[companyId]/components/ListContacts/ListContacts.tsx
+++ app/(routes)/companies/[companyId]/components/ListContacts/ListContacts.tsx
@@ -11,22 +11,51 @@ import { ListContactsProps } from "./ListContacts.types";
 
 export async function ListContacts(props: ListContactsProps) {
   const { company } = props;
-  const { userId } = auth()
+  const { userId } = auth();
 
-  if(!userId) {
-      return redirect("/")
+  if (!userId) {
+    return redirect("/");
   }
 
   const contacts = await db.contact.findMany({
     where: {
-        company: {
-            id: company.id
-        }
-    }
-  })
+      company: {
+        id: company.id,
+      },
+    },
+  });
 
   if (contacts.length === 0) {
-    return <p>You currently have no contacts</p>
+    return <p>You currently have no contacts</p>;
   }
-  return <div>ListContacts</div>;
+  return (
+    <div>
+      <div
+        className="grid items-center justify-between grid-cols-3 p-2 px-4 mt-4 mb-2
+        rounded-lg gap-x-3 bg-slate-400/20"
+      >
+        <p>Name</p>
+        <p>Role</p>
+        <p className="text-right">Contact</p>
+      </div>
+
+      {contacts.map((contact) => (
+        <div key={contact.id}>
+          <div className="grid items-center justify-between grid-cols-3 px-4 gap-x-3">
+            <p>{contact.name}</p>
+            <p>{contact.role}</p>
+            <div className="flex items-center justify-end gap-x-6">
+              <a href={`tel: ${contact.phone}`} target="_blank">
+                <Phone className="w-4 h-4"></Phone>
+              </a>
+              <a href={`mailto: ${contact.phone}`} target="_blank">
+                <Mail className="w-4 h-4"></Mail>
+              </a>
+            </div>
+          </div>
+          <Separator className="my-3" />
+        </div>
+      ))}
+    </div>
+  );
 }
