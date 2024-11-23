dd5f3546d64b6d441552d4d64bf355cd2dce5276
MiguelBG12 <miguel.bg12@gmail.com>
2024-11-23 12:17:31 -0500

    Componente ModalAddEvent con formulario en un modal que gestiona su estado

--- app/(routes)/tasks/components/ModalAddEvent/ModalAddEvent.tsx
+++ app/(routes)/tasks/components/ModalAddEvent/ModalAddEvent.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
-import { ModalAddEventProps } from './ModalAddEvent.type'
+"use client";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { ModalAddEventProps } from "./ModalAddEvent.type";
 
 export function ModalAddEvent(props: ModalAddEventProps) {
-    const {open, companies, setNewEvent, setOnSaveNewEvent, setOpen} =props
+  const { open, companies, setNewEvent, setOnSaveNewEvent, setOpen } = props;
+
   return (
-    <div>ModalAddEvent</div>
+    <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+                <DialogTitle>Add a new event</DialogTitle>
+            </DialogHeader>
+            <p>Form</p>
+        </DialogContent>
+    </Dialog>
   )
 }
