b9a0b058480023e488dc0e9ed94021109261017a
MiguelBG12 <miguel.bg12@gmail.com>
2024-11-23 12:07:17 -0500

    Desestructura propiedades en ModalAddEvent para gestionar estado y datos necesarios

---
+++ app/(routes)/tasks/components/ModalAddEvent/ModalAddEvent.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { ModalAddEventProps } from './ModalAddEvent.type'
+
+export function ModalAddEvent(props: ModalAddEventProps) {
+    const {open, companies, setNewEvent, setOnSaveNewEvent, setOpen} =props
+  return (
+    <div>ModalAddEvent</div>
+  )
+}
