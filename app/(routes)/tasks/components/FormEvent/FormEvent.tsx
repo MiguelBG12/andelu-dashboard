a4d2fdf82861da1f8e739160537a0b1f158e9f62
MiguelBG12 <miguel.bg12@gmail.com>
2024-11-23 12:45:52 -0500

    Aplica la destructuración en FormEvent para extraer las propiedades del objeto props

---
+++ app/(routes)/tasks/components/FormEvent/FormEvent.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { FormEventProps } from "./FormEvent.types";
+
+export function FormEvent(props: FormEventProps) {
+  const { companies, setNewEvent, setOnSaveNewEvent, setOpen } = props;
+  return <div>FormEvent</div>;
+}
