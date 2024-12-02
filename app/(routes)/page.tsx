89538e96fdc803831ed38ba345178d1d3f1e6fc7
MiguelBG12 <miguel.bg12@gmail.com>
2024-12-02 14:30:20 -0500

    Refactoriza Home para mover dataCardsSummary a una constante más cercana al uso

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -7,7 +7,7 @@ import { SalesDistributors } from "./components/SalesDistributors";
 import { TotalSuscribers } from "./components/TotalSuscribers";
 import { ListIntegrations } from "./components/ListIntegrations";
 
-export const dataCardsSummary = [
+const dataCardsSummary = [
   {
     icon: UserRound,
     total: "12.450",
