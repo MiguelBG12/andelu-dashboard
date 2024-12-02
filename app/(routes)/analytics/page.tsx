d622823d0a365b378e1d4c1f0d8f90e31b692a47
MiguelBG12 <miguel.bg12@gmail.com>
2024-12-02 14:29:54 -0500

    Añade CompaniesChart a PageAnalytics para visualizar datos de empresas y evento

--- app/(routes)/analytics/page.tsx
+++ app/(routes)/analytics/page.tsx
@@ -1,6 +1,7 @@
 import { redirect } from "next/navigation"
 import { auth } from "@clerk/nextjs"
 import { db } from "@/lib/db"
+import { CompaniesChart } from "./components/CompaniesChart"
 
 export default async function PageAnalytics() {
     const  {userId}= auth()
@@ -25,6 +26,11 @@ export default async function PageAnalytics() {
         }
     })
   return (
-    <div>PageAnalytics</div>
+    <div className="bg-background shadow-mb rounded-lg lg:p-4">
+        <h2 className="mb-4 text-2xl">Analytics page</h2>
+        <div>
+            <CompaniesChart companies={companies} events={events}/>
+        </div>
+    </div>
   )
 }
