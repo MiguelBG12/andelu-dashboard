152a949f492f14e0e1d1a33a1095d6ad0c72ba12
MiguelBG12 <miguel.bg12@gmail.com>
2024-12-15 18:05:02 -0500

    Depuración de archivos

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,6 @@
 import { authMiddleware } from "@clerk/nextjs/server";
 
-export default authMiddleware({
-  publicRoutes: ["/api/uploadthing"]
-});
+export default authMiddleware({});
 
 export const config = {
   matcher: [
